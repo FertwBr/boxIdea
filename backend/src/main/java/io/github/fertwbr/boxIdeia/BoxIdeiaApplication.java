77befc7a1aeb3dc506fc72dcff7115f801e92495
Fernando Vaz <fertwbr@gmail.com>
2024-10-02 21:54:29 -0300

    feat: Initial project structure

---
+++ backend/src/main/java/io/github/fertwbr/boxIdeia/BoxIdeiaApplication.java
@@ -0,0 +1,13 @@
+package io.github.fertwbr.boxIdeia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxIdeiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxIdeiaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/io/github/fertwbr/boxIdeia/BoxIdeiaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fertwbr.boxIdeia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxIdeiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

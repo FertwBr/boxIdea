34ea1ab76bf5981e756360ba54588e8818cf8db7
Fernando Vaz <fertwbr@gmail.com>
2024-10-05 14:38:28 -0300

    feat: create IdeaController

---
+++ backend/src/main/java/io/github/fertwbr/boxIdeia/controller/IdeaController.java
@@ -0,0 +1,29 @@
+package io.github.fertwbr.boxIdeia.controller;
+
+import io.github.fertwbr.boxIdeia.model.Idea;
+import io.github.fertwbr.boxIdeia.repository.IdeaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ideas")
+public class IdeaController {
+
+    @Autowired
+    private IdeaRepository ideaRepository;
+
+    @PostMapping
+    public ResponseEntity<Idea> createIdea(@RequestBody Idea idea) {
+        Idea savedIdea = ideaRepository.save(idea);
+        return new ResponseEntity<>(savedIdea, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Idea> getAllIdeas() {
+        return ideaRepository.findAll();
+    }
+}

8347ae3c699454ea3c2a76917f3dec7064f0c2c2
Fernando Vaz <fertwbr@gmail.com>
2024-10-11 11:26:57 -0300

    feat: Implements visual feedback on submission

--- frontend/Collaborate/js/Collaborate.js
+++ frontend/Collaborate/js/Collaborate.js
@@ -2,6 +2,8 @@ const tituloInput = document.querySelector('.collaborate-titulo-input');
 const nomeInput = document.querySelector('.collaborate-nome-input');
 const descricaoInput = document.querySelector('.collaborate-textarea-input');
 const enviarButton = document.querySelector('.collaborate-enviar-button');
+const successMessage = document.querySelector('.success-message');
+const errorMessage = document.querySelector('.error-message');
 
 enviarButton.addEventListener('click', () => {
     const novaIdeia = {
@@ -25,13 +27,22 @@ enviarButton.addEventListener('click', () => {
     })
     .then(data => {
         console.log('Ideia enviada com sucesso:', data);
-        alert('Ideia enviada com sucesso!');
+
+        successMessage.style.display = 'block';
+        errorMessage.style.display = 'none';
+
         tituloInput.value = '';
-        nomeInput.value = ''; 
+        nomeInput.value = '';
         descricaoInput.value = '';
+
+        setTimeout(() => {
+            successMessage.style.display = 'none';
+        }, 5000);
     })
     .catch(error => {
         console.error(error);
-        alert('Erro ao enviar ideia. Por favor, tente novamente.');
+
+        errorMessage.style.display = 'block';
+        successMessage.style.display = 'none';
     });
 });

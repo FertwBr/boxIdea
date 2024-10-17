d6c620f2154244b71ef7531846ad984d244d4151
Fernando Vaz <fertwbr@gmail.com>
2024-10-16 22:57:15 -0300

    feat: add upvote/downvote fields to Idea entity

--- backend/src/main/java/io/github/fertwbr/boxIdeia/model/Idea.java
+++ backend/src/main/java/io/github/fertwbr/boxIdeia/model/Idea.java
@@ -21,9 +21,12 @@ public class Idea {
 
     private String name;
 
+    private int upvotes = 0;
+    private int downvotes = 0;
+
     @Column(columnDefinition="TEXT")
     private String description;
-
+    
     @ElementCollection
     @CollectionTable(name = "idea_filters", joinColumns = @JoinColumn(name = "idea_id"))
     @Column(name = "filter")

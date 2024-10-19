b951d5920d6d232a9580e9cb304d55cb578122b2
Fernando Vaz <fertwbr@gmail.com>
2024-10-19 00:02:08 -0300

    feat: add experienceDate/postingDateTime fields

--- backend/src/main/java/io/github/fertwbr/boxIdeia/model/Idea.java
+++ backend/src/main/java/io/github/fertwbr/boxIdeia/model/Idea.java
@@ -7,6 +7,9 @@ import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
@@ -24,9 +27,14 @@ public class Idea {
     private int upvotes = 0;
     private int downvotes = 0;
 
+    private LocalDate experienceDate;
+
+    @Column(nullable = false)
+    private LocalDateTime postingDateTime = LocalDateTime.now();
+
     @Column(columnDefinition="TEXT")
     private String description;
-    
+
     @ElementCollection
     @CollectionTable(name = "idea_filters", joinColumns = @JoinColumn(name = "idea_id"))
     @Column(name = "filter")
